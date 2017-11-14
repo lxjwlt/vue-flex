359a7fa09557755ce00eb1f2bc5076548736dc3b
lxjwlt <lxjwlt.c@gmail.com>
2017-11-14 23:26:00 +0800

    add server command

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const {exec} = require('child_process');
 const app = express();
+const PORT = 8084;
 
 app.get('/', (req, res) => {
     res.statusCode = 200;
@@ -11,4 +13,35 @@ app.get('/', (req, res) => {
 
 app.use(express.static('docs'));
 
-app.listen(8084, () => console.log('App listening on port 8084!'));
+let cmd;
+let isExit;
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+
+    cmd = exec(`lt --port ${PORT}`);
+
+    cmd.stdout.on('data', (data) => {
+        console.log(data);
+    });
+
+    cmd.stderr.on('data', (data) => {
+        console.log(data);
+    });
+});
+
+function onExit () {
+    if (isExit) {
+        return;
+    }
+    isExit = true;
+    if (cmd) {
+        cmd.kill('SIGINT');
+        console.log('lt exit!');
+    }
+    console.log('server exit!');
+    process.exit(0);
+}
+
+process.on('exit', onExit);
+process.on('SIGINT', onExit);
